63d46e873dd52cc247242f85a6a01caee883a557
fand <mail@amagi.dev>
2023-02-02 22:23:32 +0900

    run grabbers in one thread

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let grabber0 = Grabber::new(0);
     let grabber1 = Grabber::new(1);
     grabber0.start();
-    std::thread::spawn(move || grabber1.start());
+    grabber1.start();
 
     let app = gl::GLApp::new();
     app.run();
