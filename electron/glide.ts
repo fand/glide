6d6a855a92d899fcb21ed0929472200ef462387a
fand <mail@amagi.dev>
2023-02-06 23:38:27 +0900

    refactor: move state to Glide class

---
+++ electron/glide.ts
@@ -0,0 +1,160 @@
+import { join } from "node:path";
+import { app, BrowserWindow } from "electron";
+import { globalShortcut } from "electron";
+import { Client as OSCClient, Bundle } from "node-osc";
+import { ipcMain } from "electron";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    icon: join(process.env.PUBLIC, "logo.svg"),
+    frame: false,
+    opacity: 0.9,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: true,
+      preload: join(__dirname, "./preload.js"),
+    },
+  });
+  win.setSimpleFullScreen(true);
+  //   win.webContents.openDevTools();
+
+  if (app.isPackaged) {
+    win.loadFile(join(process.env.DIST, "index.html"));
+  } else {
+    win.loadURL(process.env["VITE_DEV_SERVER_URL"]);
+  }
+
+  return win;
+}
+
+export class Glide {
+  page: number = 0;
+
+  win: BrowserWindow | undefined;
+  winNext: BrowserWindow | undefined;
+  winPrev: BrowserWindow | undefined;
+
+  transitionDict: {
+    [index: number]: {
+      transition: string;
+      duration: number;
+    };
+  } = {};
+
+  constructor(private pageCount: number) {
+    ipcMain.on("set-transition", this.onSetTransition);
+  }
+
+  onSetTransition = (
+    _e: any,
+    index: number,
+    transition: string,
+    duration: number
+  ) => {
+    console.log(">> set-transition", transition, duration);
+    this.transitionDict[index] = { transition, duration };
+  };
+
+  async init() {
+    const win = createWindow();
+    const winNext = createWindow();
+    const winPrev = createWindow();
+
+    win.title = "GLIDE-ELECTRON WIN 1";
+    winNext.title = "GLIDE-ELECTRON WIN 2";
+    winPrev.title = "GLIDE-ELECTRON WIN 0";
+
+    win.on("close", () => (this.win = undefined));
+    winNext.on("close", () => (this.winNext = undefined));
+    winPrev.on("close", () => (this.winPrev = undefined));
+
+    await new Promise((o) => win.webContents.on("did-finish-load", o));
+    await new Promise((o) => winNext.webContents.on("did-finish-load", o));
+    await new Promise((o) => winPrev.webContents.on("did-finish-load", o));
+
+    await new Promise((o) => setTimeout(o, 1000));
+
+    win.webContents.send("load", 0);
+    winNext.webContents.send("load", 1);
+    winPrev.webContents.send("load", this.pageCount - 1);
+
+    win.moveTop();
+
+    const osc = new OSCClient("127.0.0.1", 9999);
+
+    globalShortcut.register("Alt+Shift+Space", () => {
+      osc.send(new Bundle(["/init", this.pageCount, this.page]));
+    });
+
+    globalShortcut.register("Alt+Shift+Q", () => {
+      osc.send(new Bundle(["/kill"]));
+    });
+
+    globalShortcut.register("Alt+Shift+Right", async () => {
+      const oldPage = this.page;
+      this.page = (this.page + 1) % this.pageCount;
+
+      let [winPrev, win, winNext] = [this.winPrev, this.win, this.winNext];
+
+      [winPrev, win, winNext] = [win, winNext, winPrev];
+      winNext?.webContents.send("load", (this.page + 1) % this.pageCount);
+
+      win.moveTop();
+
+      const transition = this.transitionDict[oldPage];
+      osc.send(
+        new Bundle([
+          "/page",
+          oldPage,
+          this.page,
+          transition.transition,
+          transition.duration,
+        ])
+      );
+
+      [this.winPrev, this.win, this.winNext] = [winPrev, win, winNext];
+    });
+
+    globalShortcut.register("Alt+Shift+Left", () => {
+      const oldPage = this.page;
+      this.page = (oldPage - 1 + this.pageCount) % this.pageCount;
+
+      let [winPrev, win, winNext] = [this.winPrev, this.win, this.winNext];
+
+      [winPrev, win, winNext] = [winNext, winPrev, win];
+      winPrev?.webContents.send(
+        "load",
+        (this.page - 1 + this.pageCount) % this.pageCount
+      );
+
+      win.moveTop();
+
+      const transition = this.transitionDict[this.page];
+      osc.send(
+        new Bundle([
+          "/page",
+          oldPage,
+          this.page,
+          transition.transition,
+          transition.duration,
+        ])
+      );
+
+      [this.winPrev, this.win, this.winNext] = [winPrev, win, winNext];
+    });
+
+    this.win = win;
+    this.winNext = winNext;
+    this.winPrev = winPrev;
+  }
+
+  closeAll() {
+    this.win = undefined;
+    this.winNext = undefined;
+    this.winPrev = undefined;
+  }
+
+  quit() {
+    globalShortcut.unregisterAll();
+  }
+}
--- electron/main.ts
+++ electron/main.ts
@@ -1,9 +1,7 @@
 import { join } from "node:path";
 import fs from "node:fs";
 import { app, BrowserWindow } from "electron";
-import { globalShortcut } from "electron";
-import { Client as OSCClient, Bundle } from "node-osc";
-import { ipcMain } from "electron";
+import { Glide } from "./glide";
 
 // CONSTANTS
 process.env.DIST = join(__dirname, "..");
@@ -15,157 +13,17 @@ const PAGE_COUNT = fs
   .readdirSync(join(__dirname, "../../public"))
   .filter((f) => f.startsWith("page-")).length;
 
-type State = {
-  page: number;
-  win: BrowserWindow | undefined;
-  winNext: BrowserWindow | undefined;
-  winPrev: BrowserWindow | undefined;
-  transitionDict: {
-    [index: number]: {
-      transition: string;
-      duration: number;
-    };
-  };
-};
+let glide: Glide | undefined;
 
-const state: State = {
-  page: 0,
-  win: undefined,
-  winNext: undefined,
-  winPrev: undefined,
-  transitionDict: {},
-};
-
-function createWindow() {
-  const win = new BrowserWindow({
-    icon: join(process.env.PUBLIC, "logo.svg"),
-    frame: false,
-    opacity: 0.9,
-    webPreferences: {
-      contextIsolation: true,
-      nodeIntegration: true,
-      preload: join(__dirname, "./preload.js"),
-    },
-  });
-  win.setSimpleFullScreen(true);
-  //   win.webContents.openDevTools();
-
-  if (app.isPackaged) {
-    win.loadFile(join(process.env.DIST, "index.html"));
-  } else {
-    win.loadURL(process.env["VITE_DEV_SERVER_URL"]);
-  }
-
-  return win;
-}
-
-app.whenReady().then(async () => {
-  const win = createWindow();
-  const winNext = createWindow();
-  const winPrev = createWindow();
-
-  win.title = "GLIDE-ELECTRON WIN 1";
-  winNext.title = "GLIDE-ELECTRON WIN 2";
-  winPrev.title = "GLIDE-ELECTRON WIN 0";
-
-  win.on("close", () => (state.win = undefined));
-  winNext.on("close", () => (state.winNext = undefined));
-  winPrev.on("close", () => (state.winPrev = undefined));
-
-  await new Promise((o) => win.webContents.on("did-finish-load", o));
-  await new Promise((o) => winNext.webContents.on("did-finish-load", o));
-  await new Promise((o) => winPrev.webContents.on("did-finish-load", o));
-
-  await new Promise((o) => setTimeout(o, 1000));
-
-  win.webContents.send("load", 0);
-  winNext.webContents.send("load", 1);
-  winPrev.webContents.send("load", PAGE_COUNT - 1);
-
-  win.moveTop();
-
-  ipcMain.on(
-    "set-transition",
-    (e: any, index: number, transition: string, duration: number) => {
-      console.log(">> set-transition", transition, duration);
-      state.transitionDict[index] = { transition, duration };
-    }
-  );
-
-  const osc = new OSCClient("127.0.0.1", 9999);
-
-  globalShortcut.register("Alt+Shift+Space", () => {
-    osc.send(new Bundle(["/init", PAGE_COUNT, state.page]));
-  });
-
-  globalShortcut.register("Alt+Shift+Q", () => {
-    osc.send(new Bundle(["/kill"]));
-  });
-
-  globalShortcut.register("Alt+Shift+Right", async () => {
-    const oldPage = state.page;
-    state.page = (state.page + 1) % PAGE_COUNT;
-
-    let [winPrev, win, winNext] = [state.winPrev, state.win, state.winNext];
-
-    [winPrev, win, winNext] = [win, winNext, winPrev];
-    winNext?.webContents.send("load", (state.page + 1) % PAGE_COUNT);
-
-    win.moveTop();
-
-    const transition = state.transitionDict[oldPage];
-    osc.send(
-      new Bundle([
-        "/page",
-        oldPage,
-        state.page,
-        transition.transition,
-        transition.duration,
-      ])
-    );
-
-    [state.winPrev, state.win, state.winNext] = [winPrev, win, winNext];
-  });
-
-  globalShortcut.register("Alt+Shift+Left", () => {
-    const oldPage = state.page;
-    state.page = (oldPage - 1 + PAGE_COUNT) % PAGE_COUNT;
-
-    let [winPrev, win, winNext] = [state.winPrev, state.win, state.winNext];
-
-    [winPrev, win, winNext] = [winNext, winPrev, win];
-    winPrev?.webContents.send(
-      "load",
-      (state.page - 1 + PAGE_COUNT) % PAGE_COUNT
-    );
-
-    win.moveTop();
-
-    const transition = state.transitionDict[state.page];
-    osc.send(
-      new Bundle([
-        "/page",
-        oldPage,
-        state.page,
-        transition.transition,
-        transition.duration,
-      ])
-    );
-
-    [state.winPrev, state.win, state.winNext] = [winPrev, win, winNext];
-  });
-
-  state.win = win;
-  state.winNext = winNext;
-  state.winPrev = winPrev;
+app.whenReady().then(() => {
+  glide = new Glide(PAGE_COUNT);
+  glide.init();
 });
 
 app.on("window-all-closed", () => {
-  state.win = undefined;
-  state.winNext = undefined;
-  state.winPrev = undefined;
+  glide?.closeAll();
 });
 
 app.on("quit", () => {
-  globalShortcut.unregisterAll();
+  glide.quit();
 });
